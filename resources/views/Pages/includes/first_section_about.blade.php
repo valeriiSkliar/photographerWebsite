e849cd9211b54ebc863a36e241a1ae53e858f115
Yurk <Starovman@ukr.net>
2023-10-23 22:06:25 +0300

    new files for about

---
+++ resources/views/Pages/includes/first_section_about.blade.php
@@ -0,0 +1,19 @@
+<style>
+    .about_section {
+        width: 1360px;
+        height: 720px;
+        margin-top: 60px;
+    }
+</style>
+
+
+<div class="about_section section_1">
+Section 1
+</div>
+<div class="about_section section_2">
+Section 2
+</div>
+<div class="about_section section_3">
+Section 3
+</div>
+
