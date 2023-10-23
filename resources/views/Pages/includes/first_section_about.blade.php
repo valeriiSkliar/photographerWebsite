ee1e5e543291ca5bdec8b1a0ac9abe0f04eed31b
Yurk <Starovman@ukr.net>
2023-10-23 23:08:07 +0300

    about page styles

--- resources/views/Pages/includes/first_section_about.blade.php
+++ resources/views/Pages/includes/first_section_about.blade.php
@@ -1,19 +1,115 @@
 <style>
+    :root {
+        font-family: var(--navbar-font);
+    }
+
+    .name_page {
+        width: 100%;
+        height: 50px;
+        font-size: 50px;
+        text-align: center;
+        padding-top: 10px;
+    }
+
     .about_section {
         width: 1360px;
         height: 720px;
         margin-top: 60px;
+        margin-left: auto;
+        margin-right: auto;
+        position: relative;
     }
+
+    .section_1 {
+        background-image: url(assets/about_s_1.jpg);
+    }
+
+    .section_2 {
+        background-image: url(assets/about_s_2.jpg);
+    }
+
+    .section_3 {
+        background-image: url(assets/about_s_3.jpg);
+    }
+
+    .about_title {
+        position: absolute;
+        font-size: 50px;
+    }
+
+    .about_text {
+        position: absolute;
+        font-family: var(--font-allison);
+        font-size: 44px;
+    }
+
+    .title_1 {
+        color: #000000d0;
+        top: 80px;
+        left: 200px;
+    }
+
+    .text_1 {
+        color: #000000d0;
+        top: 170px;
+        left: 780px;
+    }
+
+    .title_2 {
+        color: #8e5454;
+        top: 300px;
+        left: 100px;
+    }
+
+    .text_2 {
+        color: #8e5454;
+        top: 120px;
+        left: 900px;
+    }
+
+    .title_3 {
+        color: #ffffffd0;
+        top: 50px;
+        left: 250px;
+    }
+
+    .text_3 {
+        color: #ffffffd0;
+        top: 400px;
+        left: 980px;
+    }
+
 </style>
 
-
+<div class="name_page">ABOUT MY CONCEPTS</div>
 <div class="about_section section_1">
-Section 1
+    <div class="about_title title_1">
+        Acceptance
+    </div>
+    <div class="about_text text_1">
+        My future in my hands.<br>
+        My way is beautiful.<br>
+        I`m a champion!
+    </div>
 </div>
 <div class="about_section section_2">
-Section 2
+    <div class="about_title title_2">
+        Principles
+    </div>
+    <div class="about_text text_2">
+        My future in my hands.<br>
+        My way is beautiful.<br>
+        I`m a champion!
+    </div>
 </div>
 <div class="about_section section_3">
-Section 3
+    <div class="about_title title_3">
+        Heritage
+    </div>
+    <div class="about_text text_3">
+        My future in my hands.<br>
+        My way is beautiful.<br>
+        I`m a champion!
+    </div>
 </div>
 
