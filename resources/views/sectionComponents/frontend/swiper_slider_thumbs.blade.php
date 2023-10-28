11f2ba7e1b9fbd3bfd634b0cba7fd592a246d85f
Roman Klymashenko <romanklimashenko@gmail.com>
2023-10-28 23:33:53 +0300

    change name of swiper. fix slider logic

--- resources/views/sectionComponents/frontend/swiper_slider_thumbs.blade.php
+++ resources/views/sectionComponents/frontend/swiper_slider_thumbs.blade.php
@@ -3,7 +3,7 @@
         rel="stylesheet"
         href="{{ asset('swiper/swiper-bundle.min.css') }}"
     />
-    {{-- @vite('resources/scss/swiper.scss') --}}
+    @vite('resources/scss/swiper-thumbs.scss')
 @endpushonce
 <!-- Slider main container -->
 <section class="slider">
@@ -47,16 +47,17 @@
 
 @push('custom-script')
     <script >
+        const slidesPerView = 3;
         const option = {
             loop: true,
             centeredSlides: true,
-            slidesPerView: 3,
+            slidesPerView: slidesPerView,
             spaceBetween: 200,
             speed: 500,
             autoplay: {
                 delay: 5000,
                 disableOnInteraction: false,
-            }
+            },
         };
         var galleryThumbs = new Swiper('.gallery-thumbs', {
             ...option,
@@ -68,9 +69,12 @@
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
             },
-            thumbs: {
-                swiper: galleryThumbs,
-            },
+            // thumbs: {
+            //     swiper: galleryThumbs,
+            //     autoScrollOffset: (slidesPerView - 1) / 2, //Allows to set on which thumbs active slide from edge it should automatically move scroll thumbs. For example, if set to 1 and last visible thumb will be activated (1 from edge) it will auto scroll thumbs
+
+            //     multipleActiveThumbs: false, // When enabled multiple thumbnail slides may get activated
+            // },
             ...option,
         });
 
@@ -82,7 +86,24 @@
             galleryThumbs.slidePrev();
         });
 
-        // galleryTop.controller.controls = galleryThumbs;
+        galleryTop.on('touchStart', function (){
+            this.autoplay.stop();
+            galleryThumbs.autoplay.stop();
+        });
 
+        galleryTop.on('touchEnd', function () {
+            this.autoplay.start();
+            galleryThumbs.autoplay.start();
+        });
+
+        galleryTop.on('slidePrevTransitionStart', function () {
+            galleryThumbs.slidePrev();
+        });
+        galleryTop.on('slideNextTransitionStart', function () {
+            galleryThumbs.slideNext();
+        });
+
+
+        galleryThumbs.detachEvents();
     </script>
 @endpush
