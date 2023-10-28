e03ec98f4d3c0a06f70aaa9f252ed9512604c0d2
Yurk <Starovman@ukr.net>
2023-10-28 20:36:41 +0300

    components for about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,22 +1,3 @@
-{{-- @extends('layouts.app')
-@section('metaData')
-    <x-meta-data page_id="{{$page->id}}" />
-@endsection
-@section('content')
-    @foreach($page->sections as $section)
-        @if($section->sectionComponents)
-            @foreach($section->sectionComponents as $component)
-
-                @include('sectionComponents.frontend.' . $component->template_name)
-                @if($component->album)
-                @endif
-            @endforeach
-        @endif
-    @endforeach
-@endsection --}}
-{{--@section('content')--}}
-{{--  @include('sectionComponents.frontend.'.$name)  --}}
-{{--@endsection--}}
 @extends('layouts.app')
 
 @section('content')
@@ -25,7 +6,7 @@
 @foreach($page->sections as $section)
     @if($section->components)
         @foreach($section->components as ['name'=>$name, 'album'=>$album, 'details'=>$details])
-                @include('pages.includes.first_section_about')
+                @include('sectionComponents.frontend.'.$name)
         @endforeach
     @endif
 @endforeach
