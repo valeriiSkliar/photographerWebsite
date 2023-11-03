9641c898280c77fb61ed108befdb9dd896589bb1
Romeo <104751985+semklim@users.noreply.github.com>
2023-11-03 23:22:53 +0200

    finish pages field

--- resources/views/includes/admin/page/index.blade.php
+++ resources/views/includes/admin/page/index.blade.php
@@ -18,29 +18,32 @@
                     <div class="row">
 
                         @foreach($pages as $key => $page)
-                            <div class="col-md-4 mb-4 position-relative">
-                                {{--                            <a href="{{ route('admin.pages.show', $page->id) }}" class="text-decoration-none text-white">--}}
-                                <div class="card @if($key % 2 == 0) bg-dark @else bg-secondary @endif text-light">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{{ $page->name }}</h5>
-                                        <p class="card-text"><strong>Slug:</strong> {{ $page->slug }}</p>
-                                        <div class="btn-group mt-5" role="group">
-                                            <a href="{{ route('admin.pages.edit', $page->id) }}"
-                                               class="btn btn-primary">Edit</a>
-                                            <form
-                                                class="btn-delete"
-                                                action="{{ route('admin.pages.destroy', $page->id) }}"
-                                                method="POST">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger btn-delete">
-                                                    Delete
-                                                </button>
-                                            </form>
+                            <div class="col-md-4 position-relative">
+                                    <form
+                                        class="btn-delete position-absolute"
+                                        action="{{ route('admin.pages.destroy', $page->id) }}"
+                                        method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-sm btn-danger">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </button>
+                                    </form>
+                                <a href="{{ route('admin.pages.show', $page->id) }}"
+                                   class="text-decoration-none text-white">
+                                    <div class="card @if($key % 2 == 0) bg-dark @else bg-secondary @endif text-light">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $page->name }}</h5>
+                                            <p class="card-text"><strong>Slug:</strong> {{ $page->slug }}</p>
+                                            <div class="mt-5">
+                                                <a href="{{ route('admin.pages.edit', $page->id) }}"
+                                                   class="btn btn-info" role="button">Page <i class="fas fa-edit"></i></a>
+                                                <a href="{{ route('admin.pages.edit', $page->id) }}"
+                                                   class="btn btn-warning" role="button">MetaTags <i class="fas fa-edit"></i></a>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                {{--                            </a>--}}
+                                </a>
                             </div>
                         @endforeach
 
