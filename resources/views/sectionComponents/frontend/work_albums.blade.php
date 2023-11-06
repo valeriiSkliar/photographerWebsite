7ed89e7938d67d6b98ff445ddff8abe6156e55c2
Yurk <Starovman@ukr.net>
2023-11-06 21:45:18 +0200

    work_page_start

---
+++ resources/views/sectionComponents/frontend/work_albums.blade.php
@@ -0,0 +1,14 @@
+<div class="about_section">
+    <img class="w-full h-full object-cover" src="{{ asset($album->images[0]->file_url) }}" alt="Photo">
+    <div class="about_title title_5">
+        {{$details[0]->value}}
+    </div>
+    <div class="about_text text_5">
+        @foreach($details as $text)
+            @continue($loop->first)
+            <p>
+                {{$text->value}}
+            </p>
+        @endforeach
+    </div>
+</div>
--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -3,12 +3,55 @@
     <x-meta-data page_id="" />
 @endsection
 @section('content')
-    <h1>Work</h1>
-    @foreach($page->sections as $section)
-        @if($section->sectionComponents)
-            @foreach($section->sectionComponents as $sectionComponent)
-                @include('sectionComponents.frontend.'.$sectionComponent->template_name)
-            @endforeach
-        @endif
-    @endforeach
+    @foreach($page->components as ['name'=>$name, 'album'=>$album, 'details'=>$details])
+                @include('sectionComponents.frontend.'.$name)
+@endforeach
+<div id="application_form">
+    <div class="title_form">
+
+    </div>
+    <div class="main_inputs">
+        <div class="main_inputs_details">
+            <div class="title_input">service:</div>
+            <select class="var_input">
+                <option value="wedding">wedding</option>
+                <option value="session">session</option>
+                <option value="event">event</option>
+                <option value="albums">albums</option>
+            </select>
+        </div>
+        <div class="main_inputs_details">
+            <div class="title_input">name:</div>
+            <input type="text" class="var_input">
+        </div>
+        <div class="main_inputs_details">
+            <div class="title_input">surname:</div>
+            <input type="text" class="var_input">
+        </div>
+        <div class="main_inputs_details">
+            <div class="title_input">phone:</div>
+            <input type="text" class="var_input">
+        </div>
+    </div>
+    <div title_inputs_date>
+
+    </div>
+    <div class="inputs_dates_form">
+        <div class="inputs_date">
+            <div>day</div>
+            <input type="text">
+        </div>
+        <div class="inputs_date">
+            <div>month</div>
+            <input type="text">
+        </div>
+        <div class="inputs_date">
+            <div>year</div>
+            <input type="text">
+        </div>
+    </div>
+    <div class="submit">
+        <button>Send</button>
+    </div>
+</div>
 @endsection
