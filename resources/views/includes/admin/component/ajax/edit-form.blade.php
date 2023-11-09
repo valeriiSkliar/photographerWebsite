adbc2076d5460fd5391c281e59f0bbd40f2ace2d
Valerii Skliarov <105549682+valeriiSkliar@users.noreply.github.com>
2023-11-09 22:03:47 +0200

    (fix) change SVG for <i>

--- resources/views/includes/admin/component/ajax/edit-form.blade.php
+++ resources/views/includes/admin/component/ajax/edit-form.blade.php
@@ -13,12 +13,7 @@
                 <button
                     onclick="event.stopPropagation()"
                     type="submit" class="btn btn-danger btn-sm">
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                         height="0.8em"
-                         viewBox="0 0 448 512">
-                        <path
-                            d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
-                    </svg>
+                    <i class="fa-solid fa-trash" style="color: #ffffff;"></i>
                 </button>
             </form>
         </div>
@@ -48,11 +43,7 @@
                             class="btn btn-success btn-sm mb-3"
                             id="addComponentDetail"
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
-                                <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                                <path
-                                    d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
-                            </svg>
+                            <i class="fa-solid fa-plus" style="color: #ffffff;"></i>
                         </button>
                     </div>
                 </div>
@@ -84,11 +75,7 @@
                                         href="javascript:void(0);"
                                         class="btn btn-outline-danger w-100"
                                     >
-                                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
-                                            <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                                            <path
-                                                d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
-                                        </svg>
+                                        <i class="fa-solid fa-trash" style="color: #ffffff;"></i>
                                     </button>
                                 </div>
                             </div>
@@ -141,10 +128,7 @@
                                         onclick="event.preventDefault()"
                                         class="btn btn-outline-danger"
                                     >
-                                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
-                                            <path
-                                                d="M364.2 83.8c-24.4-24.4-64-24.4-88.4 0l-184 184c-42.1 42.1-42.1 110.3 0 152.4s110.3 42.1 152.4 0l152-152c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-152 152c-64 64-167.6 64-231.6 0s-64-167.6 0-231.6l184-184c46.3-46.3 121.3-46.3 167.6 0s46.3 121.3 0 167.6l-176 176c-28.6 28.6-75 28.6-103.6 0s-28.6-75 0-103.6l144-144c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-144 144c-6.7 6.7-6.7 17.7 0 24.4s17.7 6.7 24.4 0l176-176c24.4-24.4 24.4-64 0-88.4z"/>
-                                        </svg>
+                                        <i class="fa-solid fa-paperclip" style="color: #ffffff;"></i>
                                     </button>
                                 </div>
                             </div>
