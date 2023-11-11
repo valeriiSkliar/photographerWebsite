a78cae8670d5f3b93879cc080174594010879cc4
Romeo <104751985+semklim@users.noreply.github.com>
2023-11-11 21:54:59 +0200

    fix bug in edit image

--- resources/views/includes/admin/gallery/image/edit.blade.php
+++ resources/views/includes/admin/gallery/image/edit.blade.php
@@ -1,111 +1,127 @@
 @extends('layouts.iframe')
 @section('admin.content')
-@pushonce('iframe.style')
-    <style>
-        .copy-success {
-            animation: copySuccess 0.5s ease-in-out forwards;
-            transform-origin: center;
-            transform: rotate(360deg);
-        }
-
-        .pe-none {
-            pointer-events: none;
-        }
-
-        @keyframes copySuccess {
-            0% {
-                color: #28a745;
-                transform: rotate(0deg);
-            }
-
-            100% {
-                color: #28a745;
+    @pushonce('iframe.style')
+        <style>
+            .copy-success {
+                animation: copySuccess 0.5s ease-in-out forwards;
+                transform-origin: center;
                 transform: rotate(360deg);
             }
-        }
-    </style>
-@endpushonce
-<div class="container mt-4">
-    <div class="row">
-        <div class="col-lg-10 p-4 p-md-0 m-auto">
-            <h3>Edit Image</h3>
-            <form action="{{ route('images.update', $image) }}" method="POST">
-                @csrf
-                @method('PUT')
 
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="file_url" class="col-form-label">File URL</label>
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="file_url" name="file_url" style="font-size: 12px" value="{{ config('app.url') . $image->file_url }}">
-                            <div class="input-group-append">
-                                <button class="btn btn-outline-secondary" type="button" onclick="copyToClipboard(event)"><i class="fas fa-copy pe-none"></i></button>
+            .pe-none {
+                pointer-events: none;
+            }
+
+            @keyframes copySuccess {
+                0% {
+                    color: #28a745;
+                    transform: rotate(0deg);
+                }
+
+                100% {
+                    color: #28a745;
+                    transform: rotate(360deg);
+                }
+            }
+        </style>
+    @endpushonce
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-lg-10 p-4 p-md-0 m-auto">
+                <h3>Edit Image</h3>
+                <form action="{{ route('images.update', $image) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="file_url" class="col-form-label">File URL</label>
+                            <div class="input-group">
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    id="file_url"
+                                    name="file_url"
+                                    style="font-size: 12px"
+                                    value="{{ str_contains($image->file_url, config('app.url'))
+                                              ?
+                                              $image->file_url
+                                              :
+                                              config('app.url') . $image->file_url
+                                           }}"
+                                >
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary" type="button"
+                                            onclick="copyToClipboard(event)">
+                                        <i class="fas fa-copy pe-none"></i>
+                                    </button>
+                                </div>
                             </div>
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="title" class="col-form-label">Name</label>
+                            <input type="text" class="form-control" id="title" name="title" value="{{ $image->title }}">
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="rank" class="col-form-label">Rank</label>
+                            <input type="number" class="form-control" id="rank" name="rank" value="{{ $image->rank }}">
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="alt_text" class="col-form-label">Alt Text</label>
+                            <input type="text" class="form-control" id="alt_text" name="alt_text"
+                                   value="{{ $image->alt_text }}">
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="status" class="col-form-label">Status</label>
+                            <input type="text" class="form-control" id="status" name="status"
+                                   value="{{ $image->status }}">
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="visibility" class="col-form-label">Visibility</label>
+                            <input type="text" class="form-control" id="visibility" name="visibility"
+                                   value="{{ $image->visibility }}">
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="metadata" class="col-form-label">Metadata</label>
+                            <textarea class="form-control" id="metadata" name="metadata"
+                                      rows="4">{{ $image->metadata }}</textarea>
                         </div>
                     </div>
 
-                    <div class="form-group col-md-6">
-                        <label for="name" class="col-form-label">Name</label>
-                        <input type="text" class="form-control" id="name" name="name" value="{{ $image->name }}">
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Update</button>
+                        <a href="{{ route('gallery.index') }}" class="btn btn-secondary">
+                            Back to Gallery
+                        </a>
                     </div>
-
-                    <div class="form-group col-md-6">
-                        <label for="rank" class="col-form-label">Rank</label>
-                        <input type="number" class="form-control" id="rank" name="rank" value="{{ $image->rank }}">
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <label for="alt_text" class="col-form-label">Alt Text</label>
-                        <input type="text" class="form-control" id="alt_text" name="alt_text"
-                               value="{{ $image->alt_text }}">
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <label for="status" class="col-form-label">Status</label>
-                        <input type="text" class="form-control" id="status" name="status" value="{{ $image->status }}">
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <label for="visibility" class="col-form-label">Visibility</label>
-                        <input type="text" class="form-control" id="visibility" name="visibility"
-                               value="{{ $image->visibility }}">
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <label for="metadata" class="col-form-label">Metadata</label>
-                        <textarea class="form-control" id="metadata" name="metadata"
-                                  rows="4">{{ $image->metadata }}</textarea>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Update</button>
-                    <a href="{{ route('gallery.index') }}" class="btn btn-secondary">
-                        Back to Gallery
-                    </a>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
-@pushonce('iframe.script')
-    <script>
-        function copyToClipboard(event) {
-            event.target.disabled = true;
-            /* Get the text field */
-            const copyText = document.getElementById('file_url').value;
-            /* Copy the text inside the text field */
-            navigator.clipboard.writeText(copyText);
+    @pushonce('iframe.script')
+        <script>
+            function copyToClipboard(event) {
+                event.target.disabled = true;
+                /* Get the text field */
+                const copyText = document.getElementById('file_url').value;
+                /* Copy the text inside the text field */
+                navigator.clipboard.writeText(copyText);
 
-            event.target.classList.add('copy-success');
-            event.target.classList.add('btn-outline-success');
+                event.target.classList.add('copy-success');
+                event.target.classList.add('btn-outline-success');
 
-            setTimeout(() => {
-                event.target.classList.remove('copy-success');
-                event.target.disabled = false;
-            }, 1000);
-        }
-    </script>
-@endpushonce
+                setTimeout(() => {
+                    event.target.classList.remove('copy-success');
+                    event.target.disabled = false;
+                }, 1000);
+            }
+        </script>
+    @endpushonce
 @endsection
