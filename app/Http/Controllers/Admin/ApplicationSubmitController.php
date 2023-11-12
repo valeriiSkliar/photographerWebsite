b0eb1735b108ec094a59dadb1207ea171de86ed0
Yurk <Starovman@ukr.net>
2023-11-12 11:31:24 +0200

    new page contacts

--- app/Http/Controllers/Admin/ApplicationSubmitController.php
+++ app/Http/Controllers/Admin/ApplicationSubmitController.php
@@ -10,7 +10,11 @@ class ApplicationSubmitController extends Controller
     public function submit(Request $request): \Illuminate\Http\JsonResponse
     {
         $data = $request->all();
-        //
+         $headers ='From: starovman@ukr.net' . "\r\n" .
+		'Reply-To: papapolinki@gmail.com' . "\r\n" .
+		'X-Mailer: PHP/' . phpversion();
+         $strData = implode(';/n',$data);
+        mail('starovman@ukr.net', 'order', $strData, $headers);
         return response()->json($data);
     }
 }
