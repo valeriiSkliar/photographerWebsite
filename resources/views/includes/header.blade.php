91dd6bd085934378c4e891c74ec19d9262add25a
Romeo <104751985+semklim@users.noreply.github.com>
2023-11-17 20:43:29 +0200

    make new burger menu. resolve #38

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -76,48 +76,27 @@
 </header>
 
 {{--<!-- Pop Out Navigation -->--}}
-<nav id="mobile-navigation" class="fixed top-0 right-0 bottom-0 left-0 backdrop-blur-sm z-20 text-black"
+<nav id="mobile-navigation" class="fixed top-0 right-0 bottom-0 left-0 backdrop-blur-sm z-20 text-black transition-all"
      :class="openMenu ? 'visible' : 'invisible' " x-cloak>
 
     <!-- UL Links -->
-    <ul class="absolute top-0 right-0 bottom-0 w-full py-4 bg-white drop-shadow-2xl z-20 transition-all"
-        :class="openMenu ? 'translate-x-0' : 'translate-x-full'">
+    <ul class="burgerMenuList absolute left-0 py-4 bg-black drop-shadow-2xl z-20 transition-all"
+        :class="openMenu ? 'translate-x-0' : '-translate-x-80'">
 
         @foreach($pages as ['name'=>$name, 'slug'=>$slug])
-            <li class="border-b border-inherit text-center">
+            <li class="text-center text-white burgerMenuItem">
                 <a class="block p-4" href="{{  route('page.'.$slug) }}">{{__('nav-bar.links.'.$name)}}</a>
             </li>
         @endforeach
-
-        {{--        <li class="navbar__switcher p-4 flex justify-center gap-x-12 border-b border-inherit">--}}
-        {{--            @foreach($available_locales as $locale_name => $available_locale)--}}
-        {{--                <div class="first:border-r-gray-200">--}}
-        {{--                @if($available_locale === $current_locale)--}}
-        {{--                    <span--}}
-        {{--                        class="navbar__switcher__currentLocale active__link">{{ __( 'lang-switcher.'.$locale_name) }}</span>--}}
-        {{--                @else--}}
-        {{--                    <a class="block navbar__switcher__availableLocale" href="language/{{ $available_locale }}">--}}
-        {{--                        <span>{{ __( 'lang-switcher.'.$locale_name) }}</span>--}}
-        {{--                    </a>--}}
-        {{--                @endif--}}
-        {{--                </div>--}}
-        {{--            @endforeach--}}
-        {{--        </li>--}}
-        <li class=" border-inherit flex justify-center p-10">
-            <a class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-               href="{{route('index.dashboard')}}">
-                Admin panel
-            </a>
-        </li>
     </ul>
 
     <!-- Close Button -->
-    <button class="absolute top-2 left-5 z-30 w-10" @click="openMenu = !openMenu" :aria-expanded="openMenu"
+    <button class="absolute top-0 right-0 bottom-0 left-0" @click="openMenu = !openMenu" :aria-expanded="openMenu"
             aria-controls="mobile-navigation" aria-label="Close Navigation Menu">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-             stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
-        </svg>
+{{--        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-6 w-6 absolute top-3 left-60 z-30 text-white hover:text-red-500 transition-all"--}}
+{{--             stroke="currentColor">--}}
+{{--            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>--}}
+{{--        </svg>--}}
     </button>
 
 </nav>
