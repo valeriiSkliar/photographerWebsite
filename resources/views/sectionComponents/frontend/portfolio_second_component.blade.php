b325848733cc998186a5767e642868b19cafd8af
Yurk <Starovman@ukr.net>
2023-11-18 19:54:59 +0200

    portfolio_style_2

--- resources/views/sectionComponents/frontend/portfolio_second_component.blade.php
+++ resources/views/sectionComponents/frontend/portfolio_second_component.blade.php
@@ -3,15 +3,13 @@
 <div class="portfolio_albums_list">
     @foreach ($album->images as ['file_url'=>$url])
     <div class="portfolio_albums_title" style="background-image: url({{$url}})">
-        
+        <div class="album_cover_name">
+            session
+        </div>
     </div>
     @endforeach
 </div>
 
-{{-- @foreach ($album->images as ['file_url'=>$url])
-<img class="w-full h-full object-cover" src="{{ asset($url) }}" alt="Photo">
-@endforeach
-
-@foreach ($details as ['value'=>$maker])
+{{-- @foreach ($details as ['value'=>$maker])
 {{$maker}}
-@endforeach --}}
+@endforeach  --}}
