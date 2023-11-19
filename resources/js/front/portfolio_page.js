8af30103cf58e274757b86878b38ac3067ac3e88
Yurk <Starovman@ukr.net>
2023-11-19 21:44:33 +0200

    style for portfolio

--- resources/js/front/portfolio_page.js
+++ resources/js/front/portfolio_page.js
@@ -2,6 +2,9 @@ import {getCsrfToken} from "../helpers/getCsrfToken.js"
 
 const portfolioTextCollection = document.getElementsByClassName ('portfolio_text');
 const albumCover = document.getElementsByClassName ('portfolio_albums_title');
+const sliderContainer = document.getElementById('container_for_slider');
+const scrollLeft = document.getElementById('scroll_left');
+const scrollRight = document.getElementById('scroll_right');
 
 function renderGallery (arrayURL) {
 
@@ -17,6 +20,7 @@ function renderGallery (arrayURL) {
 }
 
 function selectAlbum (index) {
+    sliderContainer.style.left = `0px`;
     const albumArray = ['wedding','session','people','nature'];
     let activeCover1 = index + 1;
     let activeCover2 = index + 2;
@@ -50,6 +54,44 @@ export async function getCurrentAlbum(albumName){
         .then(data => {console.log(data.data[0].images); renderGallery(data.data[0].images)})
 }
 
+function scrollToLeft () {
+
+    const imgCollection = document.getElementsByClassName('img_for_slider');
+    const imgCollectionLength = imgCollection.length;
+   const leftContainer = window.getComputedStyle(sliderContainer).left;
+   const widthContainer = window.getComputedStyle(sliderContainer).width;
+   const numContainer = Number(leftContainer.replace('px', ''));
+   const numContainerWidth = Number(widthContainer.replace('px', ''));
+   console.log(leftContainer)
+   const newLeftContainer = numContainer-numContainerWidth;
+   console.log(newLeftContainer);
+    if (newLeftContainer<((imgCollectionLength-1)*numContainerWidth*Math.sign(-1))) {
+         sliderContainer.style.left = `0px`;
+         return
+    };
+   sliderContainer.style.left = `${newLeftContainer}px`;
+
+}
+
+function scrollToRight () {
+
+    const imgCollection = document.getElementsByClassName('img_for_slider');
+    const imgCollectionLength = imgCollection.length;
+    const leftContainer = window.getComputedStyle(sliderContainer).left;
+    const widthContainer = window.getComputedStyle(sliderContainer).width;
+    const numContainer = Number(leftContainer.replace('px', ''));
+    const numContainerWidth = Number(widthContainer.replace('px', ''));
+    console.log(leftContainer);
+    const newLeftContainer = numContainer+numContainerWidth;
+    console.log(newLeftContainer);
+     if (newLeftContainer>0) {
+         sliderContainer.style.left = `${(imgCollectionLength-1)*numContainerWidth*Math.sign(-1)}px`;
+         return
+    };
+    sliderContainer.style.left = `${newLeftContainer}px`;
+
+}
+
 await getCurrentAlbum(selectAlbum(0));
 
 albumCover[0].addEventListener('click', async () => {
@@ -64,3 +106,5 @@ albumCover[2].addEventListener('click', async () => {
 albumCover[3].addEventListener('click', async () => {
     await getCurrentAlbum(selectAlbum(3));
 });
+scrollLeft.addEventListener('click', scrollToLeft);
+scrollRight.addEventListener('click', scrollToRight);
