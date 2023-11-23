be721e02640077b4f9cb3c80cbca87ead4cce150
Yurk <Starovman@ukr.net>
2023-11-23 22:29:29 +0200

    email for Applications

--- app/Http/Controllers/Admin/ApplicationSubmitController.php
+++ app/Http/Controllers/Admin/ApplicationSubmitController.php
@@ -6,16 +6,21 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestEmail;
+use App\Models\Contact;
 
 use function Laravel\Prompts\alert;
 
 class ApplicationSubmitController extends Controller
 {
+    public function getEmail() {
+        return Contact::find(1);
+    }
+
     public function submit(Request $request): \Illuminate\Http\JsonResponse
     {
         $data = $request->all();
          $strData = implode(';<br>',$data);
-        $recipient = 'starovman@ukr.net';
+        $recipient = $this->getEmail()->email;
         $content = $strData;
          Mail::to($recipient)->send(new TestEmail($content));
          return response()->json($data);
