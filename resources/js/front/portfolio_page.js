10861953d7bf6cb2678d795c89edccd583bf5057
Yurk <Starovman@ukr.net>
2023-12-02 23:14:11 +0200

    new logic for Portfolio slider

--- resources/js/front/portfolio_page.js
+++ resources/js/front/portfolio_page.js
@@ -7,27 +7,54 @@ const scrollLeft = document.getElementById('scroll_left');
 const scrollRight = document.getElementById('scroll_right');
 
 const portfolioSlider = {
-    position: 0,
-    width: 0
+    // position: 0,
+    // width: 0,
+    activeFrame: 0,
+    arrayWithURL:[]
 }
 
-portfolioSlider.width = Number((window.getComputedStyle(sliderContainer).width).replace('px', ''));
+// portfolioSlider.width = Number((window.getComputedStyle(sliderContainer).width).replace('px', ''));
 
-function renderGallery (arrayURL) {
+// function renderGallery (arrayURL) {
 
-    const sliderContainer = document.getElementById('container_for_slider');
-    sliderContainer.innerHTML = '';
-    arrayURL.forEach((element) => {
-        let newImg = document.createElement('img');
-        newImg.classList.add('img_for_slider');
-        newImg.setAttribute('src',element.file_url);
-        sliderContainer.append(newImg);
-    });
+//     const sliderContainer = document.getElementById('container_for_slider');
+//     sliderContainer.innerHTML = '';
+//     arrayURL.forEach((element) => {
+//         let newImg = document.createElement('img');
+//         newImg.classList.add('img_for_slider');
+//         newImg.setAttribute('src',element.file_url);
+//         sliderContainer.append(newImg);
+//     });
 
+// }
+
+function fillArrayURL (arrayURL) {
+    portfolioSlider.activeFrame = 0;
+    portfolioSlider.arrayWithURL.splice(0, portfolioSlider.arrayWithURL.length);
+    arrayURL.forEach((element)=>{
+        portfolioSlider.arrayWithURL.push(element)
+    })
+    sliderContainer.style.backgroundImage = `url(${portfolioSlider.arrayWithURL[0].file_url})`
+}
+
+function slideDirection (direction) {
+    if (direction) {
+        portfolioSlider.activeFrame += 1;
+        if (portfolioSlider.activeFrame === portfolioSlider.arrayWithURL.length) {
+            portfolioSlider.activeFrame = 0;
+        }
+    }
+    else {
+        portfolioSlider.activeFrame -= 1;
+        if (portfolioSlider.activeFrame === -1) {
+            portfolioSlider.activeFrame = portfolioSlider.arrayWithURL.length - 1;
+        }
+    }
+    sliderContainer.style.backgroundImage = `url(${portfolioSlider.arrayWithURL[portfolioSlider.activeFrame].file_url})`
 }
 
 function selectAlbum (index) {
-    sliderContainer.style.left = `0px`;
+    // sliderContainer.style.left = `0px`;
     const albumArray = ['wedding','session','people','nature'];
     let activeCover1 = index + 1;
     let activeCover2 = index + 2;
@@ -58,27 +85,30 @@ function selectAlbum (index) {
 export async function getCurrentAlbum(albumName){
     return fetch(`/api/component-get-current-album/${albumName}`, )
         .then(data => data.json())
-        .then(data => {console.log(data.data[0].images); renderGallery(data.data[0].images)})
+        .then(data => {
+            fillArrayURL(data.data[0].images);
+          //  renderGallery(data.data[0].images);
+            })
 }
 
-function scrollToX (direction) {
+// function scrollToX (direction) {
 
-    const imgCollection = document.getElementsByClassName('img_for_slider');
-    const imgCollectionLength = imgCollection.length;
-    if (direction) {
-        portfolioSlider.position += portfolioSlider.width;
-        if (portfolioSlider.position>0) {
-            portfolioSlider.position = (imgCollectionLength-1)*portfolioSlider.width*Math.sign(-1);
-        };
-    }
-    else {
-        portfolioSlider.position -= portfolioSlider.width;
-        if (portfolioSlider.position<((imgCollectionLength-1)*portfolioSlider.width*Math.sign(-1))) {
-            portfolioSlider.position = 0;
-        };
-    };
-    sliderContainer.style.left = `${portfolioSlider.position}px`;
-}
+//     const imgCollection = document.getElementsByClassName('img_for_slider');
+//     const imgCollectionLength = imgCollection.length;
+//     if (direction) {
+//         portfolioSlider.position += portfolioSlider.width;
+//         if (portfolioSlider.position>0) {
+//             portfolioSlider.position = (imgCollectionLength-1)*portfolioSlider.width*Math.sign(-1);
+//         };
+//     }
+//     else {
+//         portfolioSlider.position -= portfolioSlider.width;
+//         if (portfolioSlider.position<((imgCollectionLength-1)*portfolioSlider.width*Math.sign(-1))) {
+//             portfolioSlider.position = 0;
+//         };
+//     };
+//     sliderContainer.style.left = `${portfolioSlider.position}px`;
+// }
 
 document.addEventListener('DOMContentLoaded', async () => {
     await getCurrentAlbum(selectAlbum(0));
@@ -98,15 +128,17 @@ albumCover[3].addEventListener('click', async () => {
     await getCurrentAlbum(selectAlbum(3));
 });
 scrollLeft.addEventListener('click', () => {
-    scrollToX(0);
+   // scrollToX(0);
+    slideDirection(0);
 });
 scrollRight.addEventListener('click', () => {
-    scrollToX(1);
+   // scrollToX(1);
+    slideDirection(1);
 });
 
-window.addEventListener('resize', () => {
-    portfolioSlider.position = 0;
-    portfolioSlider.width = Number((window.getComputedStyle(sliderContainer).width).replace('px', ''));
-    sliderContainer.style.left = `0px`;
-});
+// window.addEventListener('resize', () => {
+//     portfolioSlider.position = 0;
+//     portfolioSlider.width = Number((window.getComputedStyle(sliderContainer).width).replace('px', ''));
+//     sliderContainer.style.left = `0px`;
+// });
 
--- resources/views/sectionComponents/frontend/section_thoughts.blade.php
+++ resources/views/sectionComponents/frontend/section_thoughts.blade.php
@@ -1,5 +1,5 @@
 <section class="thoughts relative mt-14">
-    <img class="w-full h-full object-cover" src="{{ asset('assets/about_s_2.jpg') }}" alt="Photo">
+    <img class="w-full h-full object-cover" src="{{ asset($album->images[0]->file_url) }}" alt="Photo">
     <h1 class="credo">
         {{$details[0]->value}}
 
