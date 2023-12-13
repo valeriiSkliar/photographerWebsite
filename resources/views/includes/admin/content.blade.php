343f75ef8042696a93f198edc51a405af0b71024
Valerii Skliarov <105549682+valeriiSkliar@users.noreply.github.com>
2023-12-13 10:18:53 +0200

    Update admin navbar styling and functionality
    This commit converts the navbar in the admin panel to a dark theme, reorganizes the link structure, and introduces flexible styling for easy component attachment and detachment.

--- resources/views/includes/admin/content.blade.php
+++ resources/views/includes/admin/content.blade.php
@@ -27,16 +27,16 @@
 {{--    </div>--}}
 {{--</div>--}}
 <div class="content-wrapper iframe-mode" data-widget="iframe" data-loading-screen="750">
-    <div class="nav navbar navbar-expand-lg navbar-white navbar-light border-bottom p-0">
-        <a class="nav-link bg-danger" href="#" data-widget="iframe-close">Close</a>
-        <a class="nav-link bg-light" href="#" data-widget="iframe-scrollleft">
-            <i
-                class="fas fa-angle-double-left">
+    <div class="nav navbar navbar-expand-lg navbar-dark navbar-light border-bottom p-0">
+        <div class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </div>
 
-            </i>
-        </a>
-        <ul class="navbar-nav" role="tablist">
-            <li class="nav-item active" role="presentation">
+        <a class="nav-link bg-danger" href="#" data-widget="iframe-close">Close</a>
+        <ul class="navbar-nav"
+            role="tablist"
+            style="display:flex; flex-direction: row; gap: 1rem">
+            <li class="nav-item active px-2" role="presentation">
                 <a class="nav-link active"
                    data-toggle="row"
                    id="tab-index"
@@ -45,8 +45,8 @@
                 </a>
             </li>
         </ul>
-        <a class="nav-link bg-light" href="#" data-widget="iframe-scrollright"><i class="fas fa-angle-double-right"></i></a>
-        <a class="nav-link bg-light" href="#" data-widget="iframe-fullscreen"><i class="fas fa-expand"></i></a>
+{{--        <a class="nav-link bg-light" href="#" data-widget="iframe-scrollright"><i class="fas fa-angle-double-right"></i></a>--}}
+{{--        <a class="nav-link bg-light" href="#" data-widget="iframe-fullscreen"><i class="fas fa-expand"></i></a>--}}
     </div>
     <div class="tab-content">
         <div
