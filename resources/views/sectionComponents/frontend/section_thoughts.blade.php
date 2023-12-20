7061eb9760890a0daa4c45daa0aaf8c441ecb9d5
Yurk <Starovman@ukr.net>
2023-12-20 19:27:17 +0200

    adaptive 2-3 main`s components

--- resources/views/sectionComponents/frontend/section_thoughts.blade.php
+++ resources/views/sectionComponents/frontend/section_thoughts.blade.php
@@ -1,4 +1,4 @@
-<section class="thoughts relative mt-14">
+{{-- <section class="thoughts relative mt-14">
     <img class="w-full h-full object-cover" src="{{ asset($album->images[0]->file_url) }}" alt="Photo">
     <h1 class="credo">
         {{$details[0]->getLocalizedTranslation()}}
@@ -11,4 +11,20 @@
             </p>
         @endforeach
     </div>
-</section>
+</section> --}}
+<div class="about_section">
+    <img class="w-full h-full object-cover" src="{{ asset($album->images[0]->file_url) }}" alt="Photo">
+    <div class="about_title title_2">
+        {{$details[0]->getLocalizedTranslation()}}
+    </div>
+    <div class="about_text text_2">
+        @foreach($details as $text)
+            @continue($loop->first)
+            <p>
+                {{$text->getLocalizedTranslation()}}
+            </p>
+        @endforeach
+    </div>
+    <div class="defense_copy"></div>
+</div>
+
