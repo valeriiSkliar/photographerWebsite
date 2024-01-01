98df5cc48b0cefdbf3a31c26a37738bf7f89254b
Yurk <Starovman@ukr.net>
2024-01-01 19:21:15 +0200

    correct email

--- resources/views/sectionComponents/frontend/portfolio_first_component.blade.php
+++ resources/views/sectionComponents/frontend/portfolio_first_component.blade.php
@@ -1,3 +1,9 @@
+<div class="main_container_for_slider">
+    <div id="container_for_slider"></div>
+    <div class="defense_copy" id="swiper_portfolio"></div>
+    <div class="button_scroll" id="scroll_left"></div>
+    <div class="button_scroll" id="scroll_right"></div>
+</div>
     @foreach ($details as $portfolio_text_for_album)
         @if (!($portfolio_text_for_album->getLocalizedTranslation() === 'not'))
             <div class="portfolio_text">
@@ -5,11 +11,6 @@
             </div>
         @endif
     @endforeach
-        <div class="main_container_for_slider">
-            <div id="container_for_slider"></div>
-            <div class="defense_copy" id="swiper_portfolio"></div>
-            <div class="button_scroll" id="scroll_left"></div>
-            <div class="button_scroll" id="scroll_right"></div>
-        </div>
+
 
 
